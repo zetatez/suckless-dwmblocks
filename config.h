a669ad4749e3f1bfd613d3dcf3feeaf3f0fc5116
Dionysus <zetatez@icloud.com>
2023-03-29 12:26:20 +0800

    cmt

--- config.h
+++ config.h
@@ -4,14 +4,14 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 /*Icon*//*Command*//*Update Interval*//*Update Signal*/
-{"", UTILS"/status-msg",              3,    0 },
+//{"", UTILS"/status-msg",              3,    0 },
 {"", UTILS"/status-apps",             1,    0 },
-{"", UTILS"/status-mail",             300,  0 },
+//{"", UTILS"/status-mail",             300,  0 },
 {"", UTILS"/status-internet",         1,    0 },
 {"", UTILS"/status-battery",          1,    0 },
 {"", UTILS"/status-volume",           1,    0 },
 {"", UTILS"/status-microphone",       1,    0 },
-{"", UTILS"/status-cpubar",           3,    0 },
+//{"", UTILS"/status-cpubar",           3,    0 },
 {"", UTILS"/status-cpu",              3,    0 },
 {"", UTILS"/status-ram",              3,    0 },
 {"", UTILS"/status-clock",            1,    0 },
