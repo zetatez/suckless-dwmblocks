a0630037414a5620f013ad1fc632855c3a4d0e8b
Dionysus <zetatez@icloud.com>
2023-03-31 18:17:17 +0800

    cmt

--- config.h
+++ config.h
@@ -4,14 +4,15 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 /*Icon*//*Command*//*Update Interval*//*Update Signal*/
-//{"", UTILS"/status-msg",              3,    0 },
+/* {"", UTILS"/status-msg",              3,    0 }, */
 {"", UTILS"/status-apps",             1,    0 },
-//{"", UTILS"/status-mail",             300,  0 },
+/* {"", UTILS"/status-mail",             300,  0 }, */
 {"", UTILS"/status-internet",         1,    0 },
 {"", UTILS"/status-battery",          1,    0 },
 {"", UTILS"/status-volume",           1,    0 },
 {"", UTILS"/status-microphone",       1,    0 },
-//{"", UTILS"/status-cpubar",           3,    0 },
+{"", UTILS"/status-screen-light",     1,    0 },
+/* {"", UTILS"/status-cpubar",           3,    0 }, */
 {"", UTILS"/status-cpu",              3,    0 },
 {"", UTILS"/status-ram",              3,    0 },
 {"", UTILS"/status-clock",            1,    0 },
@@ -19,4 +20,4 @@ static const Block blocks[] = {
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
 static char delim[] = " ";
-static unsigned int delimLen = 5;
+static unsigned int delimLen = 3;
