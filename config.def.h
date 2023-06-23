b21e76bb2ae9bf463add6f6dce5b194e6fb9183f
Dionysus <zetatez@icloud.com>
2023-06-23 17:20:55 +0800

    cmt

--- config.def.h
+++ config.def.h
@@ -1,21 +1,20 @@
 
-#define UTILS "/home/dionysus/.suckless/suckless-dwmblocks/utils"
+#define PREFIX "/home/dionysus/.suckless/suckless-dwmblocks/utils"
 
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
   /*Icon*//*Command*//*Update Interval*//*Update Signal*/
-  // {"", UTILS"/status-msg"          ,3   ,0 },
-  {"", UTILS"/status-apps"         ,1   ,0 },
-  {"", UTILS"/status-mail"         ,300 ,0 },
-  {"", UTILS"/status-net"          ,1   ,0 },
-  {"", UTILS"/status-battery"      ,1   ,0 },
-  {"", UTILS"/status-volume"       ,1   ,0 },
-  {"", UTILS"/status-micro"        ,1   ,0 },
-  {"", UTILS"/status-screen-light" ,1   ,0 },
-  // {"", UTILS"/status-cpubar"       ,3   ,0 },
-  {"", UTILS"/status-cpu"          ,3   ,0 },
-  {"", UTILS"/status-ram"          ,3   ,0 },
-  {"", UTILS"/status-clock"        ,1   ,0 },
+  // {"", PREFIX"/status-msg"          ,3   ,0 },
+  {"", PREFIX"/status-apps"         ,1   ,0 },
+  {"", PREFIX"/status-mail"         ,300 ,0 },
+  {"", PREFIX"/status-net"          ,1   ,0 },
+  {"", PREFIX"/status-battery"      ,1   ,0 },
+  {"", PREFIX"/status-volume"       ,1   ,0 },
+  {"", PREFIX"/status-micro"        ,1   ,0 },
+  {"", PREFIX"/status-screen-light" ,1   ,0 },
+  {"", PREFIX"/status-cpu"          ,3   ,0 },
+  {"", PREFIX"/status-ram"          ,3   ,0 },
+  {"", PREFIX"/status-clock"        ,1   ,0 },
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
