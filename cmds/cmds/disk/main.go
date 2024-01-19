790eb72d7d1582e73b2d56cee4f51cb8a5d49261
Dionysus <zetatez@icloud.com>
2024-01-20 00:54:56 +0800

    cmt

--- cmds/cmds/disk/main.go
+++ cmds/cmds/disk/main.go
@@ -6,13 +6,6 @@ import (
 	"cmds/sugar"
 )
 
-const (
-	BatteryPath = "/sys/class/power_supply/BAT0"
-	NetPath     = "/sys/class/net/wlan0"
-	EmailPath   = ".mail/inbox"
-	MsgPath     = ".msg"
-)
-
 func main() {
 	fmt.Println(FormatDisk())
 }
