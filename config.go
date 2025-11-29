c735d4440b8ad1c03f5da65e39091004e7d897c5
诗弋 <zetatez@icloud.com>
2025-11-29 13:27:35 +0800

    cmt

--- config.go
+++ config.go
@@ -9,9 +9,9 @@ var Delim = "  "
 
 var Blocks = []Block{
 	{Signal: 7, Interval: 5 * time.Second, Func: blocks.BlockProcs},
-	{Signal: 7, Interval: 2 * time.Second, Func: blocks.BlockBluetoothConnectedDevices},
-	{Signal: 7, Interval: 1 * time.Hour, Func: blocks.BlockWeather},
+	// {Signal: 7, Interval: 30 * time.Minute, Func: blocks.BlockWeather},
 	// {Signal: 7, Interval: 15 * time.Minute, Func: blocks.BlockEmail},
+	{Signal: 7, Interval: 2 * time.Second, Func: blocks.BlockBluetoothConnectedDevices},
 	{Signal: 7, Interval: 1 * time.Second, Func: blocks.BlockVol},
 	{Signal: 7, Interval: 1 * time.Second, Func: blocks.BlockMicro},
 	{Signal: 7, Interval: 1 * time.Second, Func: blocks.BlockScreenLight},
