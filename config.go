b6903cc3e7006303c14251ce1168ce2767ab46a8
shiyi <zetatez@icloud.com>
2026-02-23 23:06:07 +0800

    cmt

--- config.go
+++ config.go
@@ -8,7 +8,7 @@ import (
 var Delim = " "
 
 var Blocks = []Block{
-	{Interval: 400 * time.Millisecond, Func: blocks.BlockNews},
+	// {Interval: 400 * time.Millisecond, Func: blocks.BlockNews},
 	{Interval: 5 * time.Second, Func: blocks.BlockProcs},
 	// {Interval: 15 * time.Minute, Func: blocks.BlockWeather},
 	// {Interval: 15 * time.Minute, Func: blocks.BlockEmail},
