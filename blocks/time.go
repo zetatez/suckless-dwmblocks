617a32ada970e06bc1c1b669dc23d7920c555cc7
shiyi <zetatez@icloud.com>
2026-04-13 15:51:25 +0800

    cmt

--- blocks/time.go
+++ blocks/time.go
@@ -3,6 +3,6 @@ package blocks
 import "time"
 
 func BlockTime() string {
-	// 2006-01-02 15:04:05 Mon Jan/02 15:04:05 Jan/02 Mon 15:04:05 Jan/02 Mon 15:04:05
-	return time.Now().Format("Jan/02 15:04:05 ")
+	// 2006-01-02 15:04:05 Mon Jan/02
+	return time.Now().Format("Mon. Jan/02 15:04:05 ")
 }
